e28f90f00b4282e5b1abb357ea3b2cb182f0b287
eizumirika <>
2024-10-24 22:30:42 +0900

    ルーティング：新規ユーザー登録ページ/ログインページ

--- AtlasSNS_ver9/routes/auth.php
+++ AtlasSNS_ver9/routes/auth.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\PostsController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -18,3 +20,5 @@ Route::middleware('guest')->group(function () {
     Route::post('added', [RegisteredUserController::class, 'added']);
 
 });
+
+    Route::post('/top',[PostsController::class, 'index']);
