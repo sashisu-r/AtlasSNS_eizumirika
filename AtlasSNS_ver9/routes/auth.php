28beb3b753f0f6520bf7fa1e468689c8acae6a0b
eizumirika <>
2024-11-25 22:07:12 +0900

    ミドルウェアで制限

--- AtlasSNS_ver9/routes/auth.php
+++ AtlasSNS_ver9/routes/auth.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -18,8 +21,16 @@ Route::middleware('guest')->group(function () {
     Route::post('added', [RegisteredUserController::class, 'added']);
     Route::post('added', [RegisteredUserController::class, 'store']);
 
+    Route::get('top', [PostsController::class, 'index']); //topページ表示ルーティング設定
+    Route::post('top', [PostsController::class, 'index']);
 
-    //Route::get('top',[])//topページに遷移するためのルーティングを設定する！
-    //Route::post('top',[])
+});
 
+Route::middleware('auth')->group(function () { //制限
+    Route::get('top', [PostsController::class, 'index']);
+    Route::get('profile', [ProfileController::class, 'profile']);
+    Route::get('search', [UsersController::class, 'index']);
+    Route::get('follow-list', [PostsController::class, 'index']);
+    Route::get('follower-list', [PostsController::class, 'index']);
+    Route::get('user/{id}', [UsersController::class, 'show']); // 相手ユーザーのプロフィールページ
 });
