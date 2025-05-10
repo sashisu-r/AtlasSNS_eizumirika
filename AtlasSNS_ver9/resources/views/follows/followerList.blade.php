e748b9a45529ad1aabd97f76f56d731cdd68ebc3
eizumirika <>
2025-05-11 08:51:36 +0900

    フォローリスト/フォロワーリストの編集

--- AtlasSNS_ver9/resources/views/follows/followerList.blade.php
+++ AtlasSNS_ver9/resources/views/follows/followerList.blade.php
@@ -1,12 +1,24 @@
 <x-login-layout>
 
+  {{-- フォロー中のユーザーのアイコン表示 --}}
+  <div class="follow-list-top">
+    <h3>フォローリスト</h3>
+      <a href="#">
+        <img src="images/icon3.png" class="follow-list-icon">
+      </a>
+  </div>
 
-  <h2>機能を実装していきましょう。</h2>
-
-  <ul>
-    @foreach ($followers as $follower)
-        <li>{{ $follower->username }}</li>
-    @endforeach
-  </ul>
+  {{-- フォロー中ユーザーの投稿一覧表示 --}}
+  <div class="follow-list-all">
+    <div class="follow-list-content">
+      <a href="#">
+        <img src="images/icon2.png" class="follow-list-icon">
+      </a>
+      <div class="follow-text">
+        <h4>ユーザー名</h4>
+        <p>投稿内容</p>
+      </div>
+    </div>
+  </div>
 
 </x-login-layout>
