f6a4f917a6417127919b0007cee615da39e2a18f
eizumirika <>
2025-07-06 07:25:59 +0900

    ユーザー検索画面の表示

--- AtlasSNS_ver9/resources/views/users/search.blade.php
+++ AtlasSNS_ver9/resources/views/users/search.blade.php
@@ -1,11 +1,11 @@
 <x-login-layout>
 
   {{-- 検索フォーム --}}
-  <form action="{{ route('user.search') }}" method="GET" class="search">
+  <form action="{{ route('user.search') }}" method="GET" class="search-form">
     <input type="text" name="keyword" placeholder="ユーザー名" class="search-box"
            value="{{ old('keyword', $keyword ?? '') }}">
-    <button type="submit">
-      <img src="{{ asset('images/search.png') }}" alt="検索" class="search-png">
+    <button type="submit" class="search-button">
+      <img src="{{ asset('images/search.png') }}" alt="検索" class="search-icon">
     </button>
   </form>
 
@@ -15,8 +15,7 @@
       <ul>
         @foreach ($users as $user)
           <li>
-            <a href="{{ url('/profile/'.$user->id) }}">
-              {{-- ユーザーのアイコン（仮にimages/icon.png使用） --}}
+            <a href="{{ url('/profile/' . $user->id) }}">
               <img src="{{ asset('images/icon.png') }}" alt="アイコン" class="user-icon">
               {{ $user->username }}
             </a>
